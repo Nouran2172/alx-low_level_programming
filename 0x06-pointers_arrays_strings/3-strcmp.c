b8752a674e21d3fcac594357b49286f2d9effb73
Nouran2172 <N.Magdy2172@nu.edu.eg>
2023-07-27 12:03:56 -0700

    d

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,10 +4,9 @@
  * _strcmp - compare two strings
  * @s1: string 1
  * @s2: string 2
- *
  * Return: 1 if true, 0 if false
  */
-char _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	int equal = 0;
 
